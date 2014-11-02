f0b8e0a3a1391fd209a7f1e99e1a81d96595ab97
Keith de Souza <keith.desouza@genesisgroupuk.com>
2014-11-02 15:05:18 +0000

    Created home and About pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  #keith commented out the next line
+  #get 'pages/home'
+
+  #keith Added the next line
+  root "pages#home"
+  get "about" => "pages#about"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
